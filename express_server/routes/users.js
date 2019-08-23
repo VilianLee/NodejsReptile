8af4484bfd43d729aaf59f03bd7c9486cf4abc87
VilianLee <276550763@qq.com>
2019-08-23 18:36:57 +0800

    引入eslint

--- express_server/routes/users.js
+++ express_server/routes/users.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 const users = []
-router.route('/').get(function(req, res, next) {
+router.route('/').get(function (req, res, next) {
   res.json(users);
 })
-.post(function(req, res, next){
-  const user = req.body
-  users.push(user)
-  res.json(user)
-})
+  .post(function (req, res, next) {
+    const user = req.body
+    users.push(user)
+    res.json(user)
+  })
 
 module.exports = router;
