538cafdc43599b4fb402d07244b6ec2db37f772e
VilianLee <276550763@qq.com>
2019-08-23 15:44:26 +0800

    获取参数

--- simple_server.js
+++ simple_server.js
@@ -4,14 +4,14 @@ const qs = require('querystring')
 
 server.listen(8808)
 
-const users =[]
+const users = []
 
 server.on('request', (req, res) => {
     const url = req.url
-    const path = url.indexOf('?') > -1 ? url.substr(1, url.indexOf('?')) : url.substr(1, url.length) 
+    const path = url.indexOf('?') > -1 ? url.substr(1, url.indexOf('?')) : url.substr(1, url.length)
     const queryString = url.indexOf('?') > -1 ? url.substr(url.indexOf('?') + 1, url.length) : {}
     const query = qs.parse(queryString)
-
+    console.log(url)
     switch (path) {
         case 'user':
             switch (req.method) {
@@ -20,10 +20,22 @@ server.on('request', (req, res) => {
                     res.end(JSON.stringify(users))
                     break;
                 case 'POST':
-                    const user = {name: Math.floor(Math.random() * 100)}
-                    users.push(user)
-                    res.statusCode = 200
-                    res.end(JSON.stringify(users))
+                    const contentType = req.headers['content-type']
+                    if(contentType !== 'application/json'){
+                        res.statusCode = 400
+                        res.end("error")
+                    }
+                    let reqBodyString = ''
+                    req.on('data', function (data) {
+                        reqBodyString += data.toString()
+                    })
+                    req.on('end', function () {
+                        const user = JSON.parse(reqBodyString)
+                        users.push(user)
+                        res.statusCode = 200
+                        res.end(JSON.stringify(users))
+                    })
+
                     break;
                 default:
                     res.statusCode = 404
@@ -35,5 +47,3 @@ server.on('request', (req, res) => {
             break;
     }
 })
-
-const url = ""
