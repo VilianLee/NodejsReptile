93d697b1330a34cda8b78dddde8f88fb191e2bab
VilianLee <276550763@qq.com>
2019-08-23 17:27:48 +0800

    完善simple_server

--- simple_server/simple_server.js
+++ simple_server/simple_server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const server = http.createServer()
 const qs = require('querystring')
+const fs = require('fs')
 
 server.listen(8808)
 
@@ -8,29 +9,29 @@ const users = []
 
 server.on('request', (req, res) => {
     const url = req.url
-    const path = url.indexOf('?') > -1 ? url.substr(1, url.indexOf('?')) : url.substr(1, url.length)
+    const path = url.indexOf('?') > -1 ? url.substr(0, url.indexOf('?')) : url
     const queryString = url.indexOf('?') > -1 ? url.substr(url.indexOf('?') + 1, url.length) : {}
     const query = qs.parse(queryString)
-    console.log(url)
+    console.log(path)
     switch (path) {
-        case 'user':
+        case '/user':
             switch (req.method) {
                 case 'GET':
                     res.statusCode = 200
                     res.end(JSON.stringify(users))
                     break;
                 case 'POST':
-                    const contentType = req.headers['content-type']
-                    if(contentType !== 'application/json'){
-                        res.statusCode = 400
-                        res.end("error")
-                    }
+                    // const contentType = req.headers['content-type']
+                    // if(contentType !== 'application/json'){
+                    //     res.statusCode = 400
+                    //     res.end("error")
+                    // }
                     let reqBodyString = ''
                     req.on('data', function (data) {
                         reqBodyString += data.toString()
                     })
                     req.on('end', function () {
-                        const user = JSON.parse(reqBodyString)
+                        const user = qs.parse(reqBodyString)
                         users.push(user)
                         res.statusCode = 200
                         res.end(JSON.stringify(users))
@@ -42,8 +43,13 @@ server.on('request', (req, res) => {
                     break;
             }
             break;
-
+        case "/test.html":
+            res.statusCode = 200
+            fs.createReadStream('./test.html').pipe(res)
+            break
         default:
+            res.statusCode = 404
+            res.end("NOT FOUND")
             break;
     }
 })
